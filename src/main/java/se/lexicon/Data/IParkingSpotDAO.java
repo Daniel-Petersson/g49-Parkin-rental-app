a2c75d838c142b874908a6e040a1665c6a80419f
binleiden <daniel.petersson83@gmail.com>
2024-04-10 10:44:51 +0200

    Added detailed comments to methods in IReservationDAO, and IParkingSpotDAO.

--- src/main/java/se/lexicon/Data/IParkingSpotDAO.java
+++ src/main/java/se/lexicon/Data/IParkingSpotDAO.java
@@ -7,20 +7,54 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * IParkingSpotDAO is an interface for performing CRUD operations on ParkingSpot objects.
+ */
 public interface IParkingSpotDAO {
 
+    /**
+     * Creates a new parking spot and adds it to the data store.
+     * @param parkingSpot The parking spot to be added.
+     * @return The added parking spot.
+     */
     ParkingSpot create(ParkingSpot parkingSpot);
 
+    /**
+     * Finds a parking spot in the data store by its spot number.
+     * @param spotNumber The spot number of the parking spot to find.
+     * @return An Optional containing the found parking spot, or an empty Optional if no parking spot was found.
+     */
     Optional<ParkingSpot> find(String spotNumber);
 
-
+    /**
+     * Removes a parking spot from the data store by its spot number.
+     * @param spotNumber The spot number of the parking spot to remove.
+     * @return true if the parking spot was successfully removed, false otherwise.
+     */
     boolean remove(String spotNumber);
 
+    /**
+     * Finds all parking spots in the data store.
+     * @return A collection of all parking spots.
+     */
     Collection<ParkingSpot> findAll();
 
+    /**
+     * Finds parking spots by their area code.
+     * @param areaCode The area code of the parking spots.
+     * @return A list of parking spots with the given area code.
+     */
     List<ParkingSpot> findByAreaCode(int areaCode);
 
+    /**
+     * Occupies a parking spot by its spot number.
+     * @param spotNumber The spot number of the parking spot to occupy.
+     */
     void occupyParkingSPot(int spotNumber);
-    void vacateParkingSPot(int spotNumber);
 
+    /**
+     * Vacates a parking spot by its spot number.
+     * @param spotNumber The spot number of the parking spot to vacate.
+     */
+    void vacateParkingSPot(int spotNumber);
 }
--- src/main/java/se/lexicon/Data/IReservationDAO.java
+++ src/main/java/se/lexicon/Data/IReservationDAO.java
@@ -1,6 +1,5 @@
 package se.lexicon.Data;
 
-
 import se.lexicon.model.ParkingSpot;
 import se.lexicon.model.Reservation;
 
@@ -8,23 +7,63 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * IReservationDAO is an interface for performing CRUD operations on Reservation objects.
+ */
 public interface IReservationDAO {
-    //todo: impl abstract methods
+
+    /**
+     * Creates a new reservation and adds it to the data store.
+     * @param reservation The reservation to be added.
+     * @return The added reservation.
+     */
     Reservation create(Reservation reservation);
 
+    /**
+     * Finds a reservation in the data store by its id.
+     * @param id The id of the reservation to find.
+     * @return An Optional containing the found reservation, or an empty Optional if no reservation was found.
+     */
     Optional<Reservation> find(String id);
 
-
+    /**
+     * Removes a reservation from the data store by its id.
+     * @param id The id of the reservation to remove.
+     * @return true if the reservation was successfully removed, false otherwise.
+     */
     boolean remove(String id);
 
+    /**
+     * Finds a reservation by the customer's id.
+     * @param customerId The id of the customer.
+     * @return The found reservation.
+     */
     Reservation findByCustomerId(int customerId);
+
+    /**
+     * Finds a reservation by the vehicle's license plate.
+     * @param licensePlate The license plate of the vehicle.
+     * @return The found reservation.
+     */
     Reservation findByVehicleLicensePlate(String licensePlate);
+
+    /**
+     * Finds a reservation by the parking spot's number.
+     * @param spotNumber The number of the parking spot.
+     * @return The found reservation.
+     */
     Reservation findByParkingSpotNumber(int spotNumber);
 
+    /**
+     * Finds all reservations in the data store.
+     * @return A collection of all reservations.
+     */
     Collection<Reservation> findAll();
 
+    /**
+     * Finds parking spots by their area code.
+     * @param areaCode The area code of the parking spots.
+     * @return A list of parking spots with the given area code.
+     */
     List<ParkingSpot> findByAreaCode(int areaCode);
-
-
-
 }
