32ab3f2805f9a6e2bbb023812456aef92295eff1
binleiden <daniel.petersson83@gmail.com>
2024-04-10 13:17:44 +0200

    Created new test classes.

---
+++ src/test/java/se/lexicon/CustomerDAOImplTest.java
@@ -0,0 +1,57 @@
+package se.lexicon;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CustomerDAOImplTest {
+
+    private CustomerDAOImplTest testObject;
+
+    @BeforeEach
+    public void setUp() {
+        testObject = new CustomerDAOImplTest();
+    }
+
+    @Test
+    public void testCreateCustomer() {
+        //todo: Implement JUnit test
+    }
+
+    @Test
+    public void findById() {
+        //todo: Implement JUnit test
+
+    }
+
+    @Test
+    public void testNonExistentCustomer() {
+        //todo: Implement JUnit test
+
+    }
+
+    @Test
+    public void testRemoveCustomer() {
+        //todo: Implement JUnit test
+
+    }
+
+    @Test
+    public void testRemoveNonExistentCustomer() {
+        //todo: Implement JUnit test
+
+    }
+
+    @Test
+    public void testFindAllCustomers() {
+        //todo: Implement JUnit test
+
+    }
+
+    @Test
+    public void testFindAllCustomersEmptyList() {
+        //todo: Implement JUnit test
+
+    }
+
+}
+}
---
+++ src/test/java/se/lexicon/ParkingSpotDAOImplTest.java
@@ -0,0 +1,5 @@
+package se.lexicon;
+
+public class ParkingSpotDAOImplTest {
+    //todo: Implement JUnit test
+}
